9a0fc102a00e09a71212a790b35f0dd0c738f18b
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2022-01-21 23:42:47 -0300

    Melhorias e Publish IIS

--- GestaoVendasCompleto-Com-Grafico-em-M.V.C/Controllers/ClienteController.cs
+++ GestaoVendasCompleto-Com-Grafico-em-M.V.C/Controllers/ClienteController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using SistemaVendas.Models; // Importado
+using SistemaVendas.Models;
 
 namespace SistemaVendas.Controllers
 {
